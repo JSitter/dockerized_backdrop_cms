6961eb594b21cec4fc5fa99e66f43211deafa3d1
Justin Sitter <justin@jaytria.com>
2020-09-21 01:25:09 -0700

    Installer housekeeping

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 ./installer_files/unpacker.py backdrop.zip ../
 mv backdrop backdrop-src
+chmod 777 ./backdrop-src/settings.php
+mv backdrop-src/files ./files 
+mv backdrop-src/layouts ./layouts
+mv backdrop-src/modules ./modules 
+mv backdrop-src/sites ./sites 
+mv backdrop-src/themes ./themes
