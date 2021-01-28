9c3aa511d370ba5f74a4d33c15e6001c823a7642
Justin Sitter <justin@jaytria.com>
2021-01-27 17:16:15 -0800

    Prompt user for project details

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 if [ -x "$(command -v docker)" ]; then
+    read -p "Project Name: " projectName
+    read -p "Database Name: " databaseName
+    read -p "Local Port: " openPort
+    read -p "Database Username:" dbUser
+    read -p "Database Password: " dbPassword
+    sed -i "s/{backdropdb}/$projectName/" docker-compose.yml
+    sed -i "s/{appName}/$databaseName/" docker-compose.yml
+    sed -i "s/{portNumber}/$openPort/" docker-compose.yml
+    sed -i "s/{dbUser}/$dbUser/" docker-compose.yml
+    sed -i "s/{dbPassword}/$dbPassword/" docker-compose.yml
     ./source_files/downloader.py -d -i backdrop-src
     chmod 777 ./backdrop-src/settings.php
     mv backdrop-src/files ./files 
